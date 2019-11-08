b72198bc91f19b93a4537e64b02165c3bc3f5f12
Thomas Sillmann <thomas@thomassillmann.de>
2019-11-08 14:42:24 +0100

    Initial Commit

---
+++ hMD SwiftUI Vacation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hMD SwiftUI Vacation
+//
+//  Created by Thomas Sillmann on 08.11.19.
+//  Copyright © 2019 Thomas Sillmann. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
